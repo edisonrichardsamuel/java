f4a3498e15fc8f967d420c5dee2f72aef4b4871c
edisonrichardsamuel <36623763+edisonrichardsamuel@users.noreply.github.com>
2018-06-21 19:19:08 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,13 @@
+public class Fact{
+
+     public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int s=Integer.parseInt(sc.nextInt());
+        int fact=1;
+        for(int i=1;i<=s;i++)
+        {
+            fact=fact*i;
+        }
+         System.out.println(fact);
+     }
+}
